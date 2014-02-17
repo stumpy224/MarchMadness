afb6f1b4f7179c3e2eea50a4056df40c6f21187d
Jeremiah Stump <stumpy224@gmail.com>
2014-02-17 17:03:13 -0500

    Deleted users_digits

--- app/helpers/users_digits_helper.rb
+++
@@ -1,2 +0,0 @@
-module UsersDigitsHelper
-end
--- app/models/user_digit.rb
+++
@@ -1,6 +0,0 @@
-class UserDigit < ActiveRecord::Base
-	validates :user_id, presence: true
-	validates :winner_digit, presence: true
-	validates :loser_digit, presence: true
-	validates :year, presence: true
-end
