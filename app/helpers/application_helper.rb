e7db78b0c533df5966474836ebf5808301904237
Jeremiah Stump <stumpy224@gmail.com>
2014-03-11 22:35:54 -0400

    Change site title and remove ssl

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(page_title)
-    base_title = 'Grid Pool'
+    base_title = 'The Grid Pool'
     page_title.empty? ? base_title : base_title + " | " + page_title
   end
 end
